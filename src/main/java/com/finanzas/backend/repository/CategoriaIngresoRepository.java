894930630388d3ce503451e9fcfbc627d5928001
Brayan Hincapie Monsalve <hincapieb17@gmail.com>
2023-06-01 23:03:02 -0500

    Repository Categorias

---
+++ src/main/java/com/finanzas/backend/repository/CategoriaIngresoRepository.java
@@ -0,0 +1,9 @@
+package com.finanzas.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaIngresoRepository  extends JpaRepository<CategoriaIngresoRepository, Long>{
+
+}
